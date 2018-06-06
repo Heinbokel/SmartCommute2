f94cbe2860f27c67340d555655433c16266a403f
RobertHeinbokel <RobertHeinbokel@gmail.com>
2018-06-05 20:52:26 -0400

    Database updated

---
+++ SmartCommuteEmmet/Data/Migrations/20180606004959_DataAnnotationsComplete.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SmartCommuteEmmet.Data.Migrations
+{
+    public partial class DataAnnotationsComplete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commute_AspNetUsers_UserId",
+                table: "Commute");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartPointName",
+                table: "StartPoint",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartPointDescription",
+                table: "StartPoint",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndPointName",
+                table: "EndPoint",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndPointDescription",
+                table: "EndPoint",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommuteTypeName",
+                table: "CommuteType",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommuteTypeDescription",
+                table: "CommuteType",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Commute",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommuteName",
+                table: "Commute",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EndPointCustom",
+                table: "Commute",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StartPointCustom",
+                table: "Commute",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BusinessName",
+                table: "Business",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateRegistered",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserAvatar",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commute_AspNetUsers_UserId",
+                table: "Commute",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commute_AspNetUsers_UserId",
+                table: "Commute");
+
+            migrationBuilder.DropColumn(
+                name: "EndPointCustom",
+                table: "Commute");
+
+            migrationBuilder.DropColumn(
+                name: "StartPointCustom",
+                table: "Commute");
+
+            migrationBuilder.DropColumn(
+                name: "DateRegistered",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserAvatar",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartPointName",
+                table: "StartPoint",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartPointDescription",
+                table: "StartPoint",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndPointName",
+                table: "EndPoint",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndPointDescription",
+                table: "EndPoint",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommuteTypeName",
+                table: "CommuteType",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommuteTypeDescription",
+                table: "CommuteType",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Commute",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommuteName",
+                table: "Commute",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BusinessName",
+                table: "Business",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commute_AspNetUsers_UserId",
+                table: "Commute",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
