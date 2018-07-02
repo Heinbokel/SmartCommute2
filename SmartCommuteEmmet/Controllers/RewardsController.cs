7931b46ee9ff507a994de28ec6ee00a193dc6d2f
RobertHeinbokel <RobertHeinbokel@gmail.com>
2018-07-02 19:07:56 -0400

    Rewards page populates from database, grand prize checkbox added.

--- SmartCommuteEmmet/Controllers/RewardsController.cs
+++ SmartCommuteEmmet/Controllers/RewardsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +10,6 @@ using SmartCommuteEmmet.Models;
 
 namespace SmartCommuteEmmet.Controllers
 {
-    [Authorize(Roles = "Admin")]
     public class RewardsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -56,7 +54,7 @@ namespace SmartCommuteEmmet.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,RewardName,RewardDescription,RequiredCommutes,RequiredMiles")] Reward reward)
+        public async Task<IActionResult> Create([Bind("Id,RewardName,RewardDescription,RequiredCommutes,RequiredMiles,IsGrandPrize")] Reward reward)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +86,7 @@ namespace SmartCommuteEmmet.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,RewardName,RewardDescription,RequiredCommutes,RequiredMiles")] Reward reward)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RewardName,RewardDescription,RequiredCommutes,RequiredMiles,IsGrandPrize")] Reward reward)
         {
             if (id != reward.Id)
             {
