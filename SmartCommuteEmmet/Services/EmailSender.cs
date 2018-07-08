cd57cc0d10d97b72abcdc966b9918f85ef117381
RobertHeinbokel <RobertHeinbokel@gmail.com>
2018-07-08 15:20:17 -0400

    Email confirmations now send.

--- SmartCommuteEmmet/Services/EmailSender.cs
+++ SmartCommuteEmmet/Services/EmailSender.cs
@@ -1,18 +1,47 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace SmartCommuteEmmet.Services
 {
     // This class is used by the application to send email for account confirmation and password reset.
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
-    public class EmailSender : IEmailSender
+     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        private readonly IConfiguration _configuration;
+        public EmailSender(IConfiguration configuration)
         {
-            
-            return Task.CompletedTask;
+            _configuration = configuration;
+        }
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            using (var client = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = _configuration["Email:Email"],
+                    Password = _configuration["Email:Password"]
+                };
+
+                client.Credentials = credential;
+                client.Host = _configuration["Email:Host"];
+                client.Port = int.Parse(_configuration["Email:Port"]);
+                client.EnableSsl = true;
+
+                using (var emailMessage = new MailMessage())
+                {
+                    emailMessage.To.Add(new MailAddress(email));
+                    emailMessage.From = new MailAddress(_configuration["Email:Email"]);
+                    emailMessage.Subject = subject;
+                    emailMessage.Body = message;
+                    client.Send(emailMessage);
+                }
+            }
+            await Task.CompletedTask;
         }
     }
 }
