097baf5930ff4ad970782d7ca4161a61caef756a
RobertHeinbokel <RobertHeinbokel@gmail.com>
2018-07-16 13:09:36 -0400

    Added commutetype model enum and updated unsubscribe link for testing.

---
+++ SmartCommuteEmmet/Enums/CommuteTypeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartCommuteEmmet.Enums
+{
+    public class CommuteTypeModel
+    {
+        public static readonly byte Biking = 0;
+        public static readonly byte Running = 1;
+        public static readonly byte Carpooling = 2;
+    }
+}
