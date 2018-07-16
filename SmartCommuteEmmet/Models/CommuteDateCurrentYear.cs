7f3ac07f0d339537ee4000805d9b488333e49ab5
RobertHeinbokel <RobertHeinbokel@gmail.com>
2018-07-16 15:36:37 -0400

    Commute Date input is now validated for ConfigDate start and end dates.

--- SmartCommuteEmmet/Models/CommuteDateCurrentYear.cs
+++ SmartCommuteEmmet/Models/CommuteDateCurrentYear.cs
@@ -1,22 +1,31 @@
-﻿using System;
+﻿using SmartCommuteEmmet.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using SmartCommuteEmmet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SmartCommuteEmmet.Models
 {
     public class CommuteDateCurrentYear : ValidationAttribute
     {
+
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var commute = (Commute)validationContext.ObjectInstance;
+            ApplicationDbContext _context = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
 
-            if(commute.CommuteDate.Year == DateTime.Now.Year)
+            var commute = (Commute)validationContext.ObjectInstance;
+            DateTime startDate = _context.ConfigDate.Select(c => c.StartDate).FirstOrDefault();
+            DateTime endDate = _context.ConfigDate.Select(c => c.EndDate).FirstOrDefault();
+            string errorMessage = "Commute date must be between " + startDate.ToShortDateString() + " and " + endDate.ToShortDateString() + ".";
+
+            if (commute.CommuteDate >= startDate && commute.CommuteDate <= endDate)
             {
                 return ValidationResult.Success;
             }
-            return new ValidationResult("Commutes must be entered for the current year only.");
+            return new ValidationResult(errorMessage);
         }
     }
 }
